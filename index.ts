25c52d852b6019233b1a48d64624dab824f85d59
y77cao <yujiac8000@gmail.com>
2023-09-02 22:53:03 -0400

    wip

--- index.ts
+++ index.ts
@@ -14,10 +14,11 @@ const positions: {
   [address: string]: {
     shares: number;
     cost: string; // stored as string since BigNumber is not serializable and can be written to file
+    createdAt: number;
   };
 } = {};
 
-const contractAddress = "0xCF205808Ed36593aa40a44F10c7f7C2F67d4A4d4"; // "0x8aAC5570d54306Bb395bf2385ad327b7b706016b"
+const contractAddress = "0xf201fFeA8447AB3d43c98Da3349e0749813C9009"; //"0xCF205808Ed36593aa40a44F10c7f7C2F67d4A4d4";
 const provider = new ethers.providers.WebSocketProvider(
   process.env.BASE_WS_URL
 );
@@ -30,13 +31,17 @@ const contract = new ethers.Contract(
 const friendTechClient = new FriendtechClient();
 const twitterClient = new TwitterClient();
 const ws = new WebSocket(process.env.BASE_WS_URL);
+let subscriptionId = null; // newPendingTransactions subscription ID
 
 const saveToFile = async (data: object) => {
   try {
     await fs.promises.writeFile(
       `${__dirname}/positions.json`,
       JSON.stringify(data, null, 2),
-      { flag: "a+" } // append or create
+      {
+        encoding: "utf-8",
+        flag: "w",
+      }
     );
   } catch (err) {
     console.log(`Save to file failed`, err);
@@ -44,24 +49,27 @@ const saveToFile = async (data: object) => {
 };
 
 const snipe = async (txInfo: ethers.providers.TransactionResponse) => {
-  const { from, to, value } = txInfo;
+  const { from: caseSensitiveFrom, to, value } = txInfo;
+  // for maybeExit later, we need to check the calldata with this given address
+  // which is not case sensitive, so we convert it to lowercase here
+  const from = caseSensitiveFrom.toLowerCase();
 
   if (to !== contractAddress || !value.eq(0)) return;
 
-  const frientTechUserInfo = await friendTechClient.getUserByAddress(from);
-  const twitterUserInfo = await twitterClient.getUser(
-    frientTechUserInfo.twitterUsername
-  );
+  // const frientTechUserInfo = await friendTechClient.getUserByAddress(from);
+  // const twitterUserInfo = await twitterClient.getUser(
+  //   frientTechUserInfo.twitterUsername
+  // );
 
-  if (!twitterUserInfo || twitterUserInfo.follower_count < 10) return;
+  // if (!twitterUserInfo || twitterUserInfo.follower_count < 10) return;
 
   console.log(
     "Actionable account",
     JSON.stringify(
       {
         ...txInfo,
-        ...frientTechUserInfo,
-        followerCount: twitterUserInfo.followers_count,
+        // ...frientTechUserInfo,
+        // followerCount: twitterUserInfo.followers_count,
       },
       null,
       2
@@ -71,7 +79,7 @@ const snipe = async (txInfo: ethers.providers.TransactionResponse) => {
   const cost = await contract.getBuyPriceAfterFee(from, 1);
   const formattedCost = ethers.utils.formatEther(cost);
   console.log(
-    `Estimate cost to snipe user ${frientTechUserInfo.twitterUsername}: `,
+    // `Estimate cost to snipe user ${frientTechUserInfo.twitterUsername}: `,
     formattedCost
   );
 
@@ -87,20 +95,20 @@ const snipe = async (txInfo: ethers.providers.TransactionResponse) => {
     const receipt = await buyTx.wait();
     const txCost = receipt.gasUsed.mul(receipt.effectiveGasPrice).add(cost);
     const formattedTxCost = ethers.utils.formatEther(txCost);
-
     if (!positions[from]) {
       positions[from] = {
         shares: 0,
         cost: "0",
+        createdAt: Date.now(),
       };
     }
     positions[from] = {
       shares: positions[from].shares + 1,
       cost: BigNumber.from(positions[from].cost).add(txCost).toString(),
+      createdAt: Date.now(),
     };
-
     console.log(
-      `Sniped 1 share of user ${frientTechUserInfo.twitterUsername} with cost ${formattedTxCost} ETH`
+      `Sniped 1 share of user ` //${frientTechUserInfo.twitterUsername} with cost ${formattedTxCost} ETH`
     );
   } catch (err) {
     console.log(`Snipe failed`, err);
@@ -109,9 +117,47 @@ const snipe = async (txInfo: ethers.providers.TransactionResponse) => {
 
 const maybeExit = async (txInfo: ethers.providers.TransactionResponse) => {
   const { from, to, data: txData } = txInfo;
-  // const sellTx = await contract.sellShares(from, 1);
-  // await sellTx.wait();
-  // console.log("sellTx", sellTx);
+  const subject = `0x${txData.slice(34, 74)}`;
+
+  console.log("subject", subject);
+
+  const holdingSet = new Set(
+    Object.keys(positions).map((k) => k.toLowerCase())
+  );
+
+  console.log({
+    holdingSet,
+    to,
+    contractAddress,
+    sellable: holdingSet.has(subject),
+  });
+  if (to !== contractAddress || !holdingSet.has(subject)) return;
+
+  console.log({ positions });
+  const { shares, cost, createdAt } = positions[subject];
+
+  try {
+    const sellPrice: BigNumber = await contract.getSellPriceAfterFee(
+      subject,
+      shares
+    );
+    // TODO consider gas spend
+    const formattedSellPrice = ethers.utils.formatEther(sellPrice);
+    const prevCost = BigNumber.from(cost);
+    const holdTime = Date.now() - createdAt;
+    const oneDay = 1000 * 60 * 60 * 24;
+
+    console.log({ sellPrice, prevCost, formattedSellPrice, holdTime });
+
+    // don't sell if price is lower than cost and if hold time is less than 1 day
+    if (sellPrice.lte(prevCost) && holdTime < oneDay) return;
+
+    const sellTx = await contract.sellShares(subject, shares);
+    await sellTx.wait();
+    console.log("sellTx", sellTx);
+  } catch (err) {
+    console.log(`Get sell price failed`, err);
+  }
 };
 
 const main = async () => {
@@ -123,6 +169,8 @@ const main = async () => {
     Object.assign(positions, JSON.parse(savedPositions));
   }
 
+  const requestId = Date.now();
+
   ws.on("open", () => {
     console.log("Websocket connected to node");
 
@@ -130,12 +178,10 @@ const main = async () => {
       jsonrpc: "2.0",
       method: "eth_subscribe",
       params: ["newPendingTransactions"],
-      id: 1,
+      id: requestId,
     };
 
     ws.send(JSON.stringify(subscriptionRequest));
-
-    console.log("Subscribed to newPendingTransactions");
   });
 
   ws.on("close", () => {
@@ -143,22 +189,27 @@ const main = async () => {
   });
 
   ws.on("message", async (data) => {
-    const { method, params } = JSON.parse(data.toString());
+    const { method, params, result, id } = JSON.parse(data.toString());
+    if (id === requestId) {
+      console.log("Subscribed to newPendingTransactions");
+      subscriptionId = result;
+      return;
+    }
     if (method !== "eth_subscription") return;
 
     const hash = params.result;
     const txInfo = await provider.getTransaction(hash);
     if (!txInfo) return;
 
+    console.log("txInfo", txInfo);
+
     const { data: txData } = txInfo;
     const contractMethod = txData.slice(0, 10);
 
     if (contractMethod === methodSignatures.buyShares) {
       await snipe(txInfo);
-      await maybeExit(txInfo);
-    } else if (contractMethod === methodSignatures.sellShares) {
-      await maybeExit(txInfo);
     }
+    await maybeExit(txInfo);
   });
 };
 
